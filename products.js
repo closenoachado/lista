f4272832d498789e46baecd64c125d149d603fec
closenoachado <atendimentopromotoken@gmail.com>
2025-05-17 17:16:27 -0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,72 @@
+const products = [
+            {
+                id: '510',
+                name: 'teste',
+                image: 'https://i.imgur.com/gvbRopI.jpeg',
+                category: 'acessórios',
+                affiliateLink: '#'
+            },
+            {
+                id: '509',
+                name: 'penteadeira olívia',
+                image: 'https://i.imgur.com/gvbRopI.jpeg',
+                category: 'utilidades',
+                affiliateLink: 'https://s.shopee.com.br/2fyHQxeXwG'
+            },
+            {
+                id: '508',
+                name: 'creme corretivo mágic',
+                image: 'https://i.imgur.com/VCxjUwB.png',
+                category: 'beleza',
+                affiliateLink: 'https://s.shopee.com.br/9ABipVQvvF'
+            },
+            {
+                id: '507',
+                name: 'sheglam blush líquido',
+                image: 'https://i.imgur.com/Q253IHe.png',
+                category: 'beleza',
+                affiliateLink: 'https://s.shopee.com.br/30b5MLhw8o'
+            },
+            {
+                id: '506',
+                name: 'vestido de verão',
+                image: 'https://i.imgur.com/ZLfYU8I.jpeg',
+                category: 'moda',
+                affiliateLink: 'https://s.shopee.com.br/40TabsMSKp'
+            },
+            {
+                id: '505',
+                name: 'tênis moleca original',
+                image: 'https://i.imgur.com/ZliilUP.jpeg',
+                category: 'moda',
+                affiliateLink: 'https://s.shopee.com.br/7fMsqpI81E'
+            },
+            {
+                id: '504',
+                name: 'tênis feminino casual',
+                image: 'https://i.imgur.com/za1Dm0N.jpeg',
+                category: 'moda',
+                affiliateLink: 'https://s.shopee.com.br/7pgHcMfmqS'
+            },
+            {
+                id: '503',
+                name: 'vestido feminino festa',
+                image: 'https://i.imgur.com/dRqngeY.jpeg',
+                category: 'moda',
+                affiliateLink: 'https://s.shopee.com.br/1qP4NnVaqc'
+            },
+            {
+                id: '502',
+                name: 'gloss kiko milano',
+                image: 'https://i.imgur.com/UCTxbLK.jpeg',
+                category: 'beleza',
+                affiliateLink: 'https://s.shopee.com.br/VtgTRgN80'
+            },
+            {
+                id: '501',
+                name: 'bloom oil flores',
+                image: 'https://i.imgur.com/1EYMNL6.jpeg',
+                category: 'beleza',
+                affiliateLink: 'https://s.shopee.com.br/6VAtZJsdda'
+            }
+        ];
