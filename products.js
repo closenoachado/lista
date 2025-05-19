4c8bba2809a02bee6f93166348beb24edfd7c7de
closenoachado <atendimentopromotoken@gmail.com>
2025-05-19 11:14:51 -0300

    Update products.js

--- products.js
+++ products.js
@@ -1,72 +1,82 @@
 const products = [
             {
                 id: '510',
-                name: 'teste',
-                image: 'https://i.imgur.com/gvbRopI.jpeg',
-                category: 'acessórios',
-                affiliateLink: '#'
+                name: 'kit pinceis de maquiagem',
+                image: 'https://i.imgur.com/JZEHiWI.png',
+                category: 'beleza',
+                affiliateLink: 'https://www.mercadolivre.com.br/kit-de-pinceis-de-maquiagem-pincel-make-profissional-shein-cor-lilas/p/MLB45556990',
+                marketplace: 'mercadolivre' // Special tag for this product
             },
             {
                 id: '509',
                 name: 'penteadeira olívia',
                 image: 'https://i.imgur.com/gvbRopI.jpeg',
                 category: 'utilidades',
-                affiliateLink: 'https://s.shopee.com.br/2fyHQxeXwG'
+                affiliateLink: 'https://s.shopee.com.br/2fyHQxeXwG',
+                marketplace: 'shopee'
             },
             {
                 id: '508',
                 name: 'creme corretivo mágic',
                 image: 'https://i.imgur.com/VCxjUwB.png',
                 category: 'beleza',
-                affiliateLink: 'https://s.shopee.com.br/9ABipVQvvF'
+                affiliateLink: 'https://s.shopee.com.br/9ABipVQvvF',
+                marketplace: 'shopee'
             },
             {
                 id: '507',
                 name: 'sheglam blush líquido',
                 image: 'https://i.imgur.com/Q253IHe.png',
                 category: 'beleza',
-                affiliateLink: 'https://s.shopee.com.br/30b5MLhw8o'
+                affiliateLink: 'https://s.shopee.com.br/30b5MLhw8o',
+                marketplace: 'shopee'
             },
             {
                 id: '506',
                 name: 'vestido de verão',
                 image: 'https://i.imgur.com/ZLfYU8I.jpeg',
                 category: 'moda',
-                affiliateLink: 'https://s.shopee.com.br/40TabsMSKp'
+                affiliateLink: 'https://s.shopee.com.br/40TabsMSKp',
+                marketplace: 'shopee'
             },
             {
                 id: '505',
                 name: 'tênis moleca original',
                 image: 'https://i.imgur.com/ZliilUP.jpeg',
                 category: 'moda',
-                affiliateLink: 'https://s.shopee.com.br/7fMsqpI81E'
+                affiliateLink: 'https://s.shopee.com.br/7fMsqpI81E',
+                marketplace: 'shopee'
             },
             {
                 id: '504',
                 name: 'tênis feminino casual',
                 image: 'https://i.imgur.com/za1Dm0N.jpeg',
                 category: 'moda',
-                affiliateLink: 'https://s.shopee.com.br/7pgHcMfmqS'
+                affiliateLink: 'https://s.shopee.com.br/7pgHcMfmqS',
+                marketplace: 'shopee'
             },
             {
                 id: '503',
                 name: 'vestido feminino festa',
                 image: 'https://i.imgur.com/dRqngeY.jpeg',
                 category: 'moda',
-                affiliateLink: 'https://s.shopee.com.br/1qP4NnVaqc'
+                affiliateLink: 'https://s.shopee.com.br/1qP4NnVaqc',
+                marketplace: 'shopee'
             },
             {
                 id: '502',
                 name: 'gloss kiko milano',
                 image: 'https://i.imgur.com/UCTxbLK.jpeg',
                 category: 'beleza',
-                affiliateLink: 'https://s.shopee.com.br/VtgTRgN80'
+                affiliateLink: 'https://s.shopee.com.br/VtgTRgN80',
+                marketplace: 'shopee'
             },
             {
                 id: '501',
                 name: 'bloom oil flores',
                 image: 'https://i.imgur.com/1EYMNL6.jpeg',
                 category: 'beleza',
-                affiliateLink: 'https://s.shopee.com.br/6VAtZJsdda'
+                affiliateLink: 'https://s.shopee.com.br/6VAtZJsdda',
+                marketplace: 'shopee'
             }
         ];
